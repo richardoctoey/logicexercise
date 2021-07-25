5f3c023d709ba7605f18be35a12994591ed6f539
richard <myrazel@gmail.com>
2021-07-26 00:41:14 +0700

    update

--- main.go
+++ main.go
@@ -12,10 +12,10 @@ func main() {
 	walls := flag.String("w", "{\"walls\":[[0,1]]}", "Json of Walls")
 	flag.Parse()
 	box := logic.GenerateLinkedList(*row, *col)
+	theWalls := logic.ParseJson(*walls).Walls
 	//logic.SetWall(box, [][]int{{0,0}, {0,1}, {0,2}, {2,1}, {3,0}, {3,1}, {3,2}}, row, col)
-	fmt.Println(logic.ParseJson(*walls).Walls)
-	logic.SetWall(box, logic.ParseJson(*walls).Walls, *row, *col)
+	logic.SetWall(box, theWalls, *row, *col)
 	totalGeneratedGunman := logic.SetRandomGunMan(box, *row, *col)
-	fmt.Println("totalGeneratedGunman: ", totalGeneratedGunman)
 	logic.Print(box)
+	fmt.Println("totalGeneratedGunman: ", totalGeneratedGunman)
 }
